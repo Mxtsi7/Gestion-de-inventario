dd5e5065281d4fc46b1ed4f49966299bdda1ef58
Mxtsi7 <maxisaezb@gmail.com>
2026-04-12 18:35:45 -0400

    fix: elimina ProductDto duplicado de GetProductList, usar DTOs.ProductDto como canonico

--- InventoryControl/InventoryControl.Application/Queries/GetProductList/ProductDto.cs
+++
@@ -1,3 +0,0 @@
-// Este archivo fue eliminado: usar InventoryControl.Application.DTOs.ProductDto como DTO canónico.
-// Se mantiene vacío para no romper referencias de namespace en el handler.
-namespace InventityControl.Application.Queries.GetProductList;
