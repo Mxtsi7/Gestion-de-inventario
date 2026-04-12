21638e0609201842817628a676f9cf3ec1800e69
Mxtsi7 <maxisaezb@gmail.com>
2026-04-12 17:12:17 -0400

    fix: corrige ruta productForm en AppShell para que coincida con la navegación en ProductosViewModel

--- InventoryControl/InventoryControl.UI/AppShell.xaml.cs
+++ InventoryControl/InventoryControl.UI/AppShell.xaml.cs
@@ -7,6 +7,6 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-        Routing.RegisterRoute(nameof(ProductFormPage), typeof(ProductFormPage));
+        Routing.RegisterRoute("productForm", typeof(ProductFormPage));
     }
 }
